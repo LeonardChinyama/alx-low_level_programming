5920981f62efb9121b028121761445936c95861f
Leonard Kapokolo Chinyama <77678637+LeonardChinyama@users.noreply.github.com>
2023-02-27 22:29:37 +0200

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,11 +7,11 @@ void puts_half(char *str)
 {
     int i, t;
 
-    for(i = 0; str[i] != '\0'; i++)
+    for(i = 0; str[i] != 0; i++)
     {
     }
 
-    for(t = i / 2; t <= i; t++){
+    for(t = i / 2; t < i; t++){
         _putchar(str[t]);
     }
     _putchar('\n');
