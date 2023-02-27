faeb4e0ffde1e4a564746b60c65aade81481affe
Leonard Kapokolo Chinyama <77678637+LeonardChinyama@users.noreply.github.com>
2023-02-27 22:02:13 +0200

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,10 +5,11 @@
 */
 void _puts(char *str)
 {
-    int i;
-    for(i = 0; str[i] !=0; i++)
-    {
-        putchar(str[i]);
-    }
-    putchar('\n');
+	int c;
+
+	for (c = 0; str[c] != 0; c++)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
 }
