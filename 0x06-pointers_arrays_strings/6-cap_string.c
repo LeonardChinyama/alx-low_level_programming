abcfecfbe323e3f865396b393aba712569177ea2
LeonardChinyama <leonardkapokolo@gmail.com>
2023-03-02 23:49:34 +0200

    N

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,21 +6,21 @@ include "main.h"
 char *cap_string(char *f)
 {
    int i;
-   int pre_was_space = 0;
+   int sp = 0;
 
     for(i = 0; f[i] != '\0'; i++)
     {
         int a = i - 1;
         if( i != 0 && f[a] != '-' && ((f[a] < 'a' && f[a] > 'Z') || f[a] < 'A' || f[a] > 'z'))
         {
-            pre_was_space = 1;
+            sp = 1;
 
-        } else if(pre_was_space == 1 && (f[a] >= 'a' && f[a] <= 'z'))
+        } else if(sp == 1 && (f[a] >= 'a' && f[a] <= 'z'))
         {
             f[a] = f[a] - 32;
-            pre_was_space = 0;
+            sp = 0;
         }else{
-            pre_was_space = 0;
+            sp = 0;
         }
     }
     return (f);
