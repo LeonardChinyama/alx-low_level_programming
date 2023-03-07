1116caab0022e71dba1fde44f4d9df58052afe41
Leonard Kapokolo Chinyama <77678637+LeonardChinyama@users.noreply.github.com>
2023-03-07 16:41:31 +0200

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,8 +10,8 @@ void print_diagsums(int *a, int size)
 	unsigned int i, j, s1, s2;
 	s1 = 0;
 	s2 = 0;
-	for (i = 0; i < size; i++)
-		s1 += a[(i * size + i)];
+	for (i = 0; i <= (size * size); i = i + size + 1)
+		s1 += a[i];
 	for (j = size - 1; j <= (size * size) - size; j = j + size - 1)
 		s2 += a[j];
 	printf("%d, %d\n", s1, s2);
